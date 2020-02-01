da7cec82edf5b827cd8c66f8ae6c844870dc8d81
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-02-01 15:41:07 +0300

    styling-the-hero-section

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-//Select element function
+//click toggle effect
+
+//select element function
+//A Query selector function that will be called to select elements
+const selectElement = element => {
+  return document.querySelector(element);
+};
+
+let menuToggler = selectElement(".menu-toggle");
+let body = selectElement("body");
+
+//adding a listerner
+menuToggler.addEventListener("click", function() {
+  body.classList.toggle("open");
+});
